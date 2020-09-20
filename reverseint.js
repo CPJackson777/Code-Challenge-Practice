d966e338ebbe03c988de1cf10d60b46538f5226b
Charles Jackson <cpjackson572@gmail.com>
2020-09-19 22:51:30 -0500

    option added

--- reverseint.js
+++ reverseint.js
@@ -15,3 +15,10 @@
 
 // Note:
 // Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+const reverse = x => {
+    const limit = 2147483648;
+    const k = x < 0 ? -1 : 1;
+    const n = Number(String(Math.abs(x)).split('').reverse().join(''));
+    return n > limit ? 0 : n * k;
+};
